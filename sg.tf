1723dc7990f13e9f86c7045a732baaf8cfb1c5c9
Janos Pasztor <janos@pasztor.at>
2020-10-17 23:11:30 +0200

    Added Prometheus from admin IP only

--- sg.tf
+++ sg.tf
@@ -16,11 +16,12 @@ resource "exoscale_security_group_rule" "http" {
 resource "exoscale_security_group_rule" "prometheus" {
   security_group_id = exoscale_security_group.autoscaling.id
   type = "INGRESS"
-  cidr = "0.0.0.0/0"
+  cidr = var.admin_ip
   start_port = "9090"
   end_port = "9090"
   protocol = "TCP"
   description = "Managed by Terraform!"
+  count = var.admin_ip==""?0:1
 }
 
 resource "exoscale_security_group_rule" "internal" {
