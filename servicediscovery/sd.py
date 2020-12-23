a271e7ade689539d935f48a046d161a7969862e0
Janos Pasztor <janos@pasztor.at>
2020-12-23 20:26:34 +0100

    TERM handler

--- servicediscovery/sd.py
+++ servicediscovery/sd.py
@@ -1,6 +1,7 @@
 import json
 import os
 import pprint
+import signal
 import time
 
 import exoscale
@@ -13,7 +14,8 @@ def service_discovery(exo: Exoscale, zone: Zone, pool_id: str, port: int, file: 
     instances = instance_pool.instances
     targets = []
     for instance in instances:
-        targets.append(instance.ipv4_address + ":" + str(port))
+        if instance.ipv4_address is not None:
+            targets.append(instance.ipv4_address + ":" + str(port))
 
     with open(file, "w") as f:
         f.write(json.dumps(
@@ -26,6 +28,17 @@ def service_discovery(exo: Exoscale, zone: Zone, pool_id: str, port: int, file: 
         ))
 
 
+finished = False
+
+
+def finish(signum, frame):
+    global finished
+    finished = True
+
+
+signal.signal(signal.SIGTERM, finish)
+
+
 def loop_service_discovery(
         key: str,
         secret: str,
@@ -34,22 +47,23 @@ def loop_service_discovery(
         port: int,
         file: str
 ):
+    global finished
     exo = exoscale.Exoscale(api_key=key, api_secret=secret, config_file="")
 
     zone = exo.compute.get_zone(zone_name)
 
     failed_sd = 0
-    while True:
-        if failed_sd > 3:
+
+    while not finished:
+        if failed_sd > 10:
             raise Exception("Too many service discovery failures.")
         try:
             service_discovery(exo, zone, pool_id, port, file)
             failed_sd = 0
-            time.sleep(10)
         except Exception as err:
             pprint.pprint(err)
             failed_sd = failed_sd + 1
-            time.sleep(30)
+        time.sleep(5)
 
 
 if __name__ == "__main__":
